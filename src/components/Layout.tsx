dc59e7a482dfc8c8a96fdbee6c2273597cc769e9
yingyi.chow <yingyi.chow@gmail.com>
2026-05-15 08:07:04 +0000

    feat: Add Disqus comment section to the bottom of the layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,31 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Sidebar from './Sidebar';
+import { DiscussionEmbed } from 'disqus-react';
 
 function Layout() {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   return (
     <div className="flex min-h-screen bg-surface">
       <Sidebar />
-      <main className="flex-1 ml-64 p-margin-desktop w-full">
-        <Outlet />
+      <main className="flex-1 ml-64 p-margin-desktop w-full flex flex-col">
+        <div className="flex-1">
+          <Outlet />
+        </div>
+        <div className="mt-16 pt-8 border-t border-outline-variant max-w-container-max mx-auto w-full">
+          <DiscussionEmbed
+            shortname='medclaim'
+            config={
+                {
+                    url: `https://medclaim.com${currentPath}`,
+                    identifier: currentPath,
+                    title: `MedClaim - ${currentPath.replace('/', '')}`,
+                    language: 'zh_TW' //e.g. for Traditional Chinese (Taiwan)
+                }
+            }
+          />
+        </div>
       </main>
     </div>
   );
